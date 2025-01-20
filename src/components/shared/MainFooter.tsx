0ba0e6e08cb85b617597c8193ccd36f0afe98db9
Jefferson <stupidjeff430@gmail.com>
2025-01-20 17:52:21 +0700

    chore: update dependencies

--- src/components/shared/MainFooter.tsx
+++ src/components/shared/MainFooter.tsx
@@ -32,7 +32,7 @@ export default function MainFooter() {
   return (
     <footer className='select-none mx-auto my-8 max-w-[872px] bottom-0 px-4 text-stone-300 md:px-0'>
       <p className='text-gray-500 text-center dark:text-gray-400'>
-        &copy; {year}&nbsp; RJ Jefferson
+        &copy;2024-{year}&nbsp; RJ Jefferson
       </p>
       <nav role='navigation'>
         <ul className='flex flex-row justify-center gap-2 mt-1 text-lg'>
